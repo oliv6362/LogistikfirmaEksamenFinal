aeaf2e164c4a807b2c82481125fadca7a0561a3a
Wrenchx <wzs84ahh@gmail.com>
2023-05-10 14:09:51 +0200

    DBcontroller og usecase funktioner

--- src/main/java/DB/DBController.java
+++ src/main/java/DB/DBController.java
@@ -1,4 +1,51 @@
 package DB;
 
+import Entity.User;
+
+import java.sql.*;
+
 public class DBController {
+
+    public Connection connection;
+
+    private Statement stmt;
+
+    public DBController() {
+        connection = null;
+        stmt = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/haslevdk", "root", "1234");
+
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    public User getUser(int Id, fName, lName, password) {
+        try {
+            User user = new User();
+
+            String sql = "SELECT * FROM User WHERE userID = " + Id + ";";
+            Statement stmt = connection.createStatement();
+            stmt.execute(sql);
+
+            ResultSet rs = stmt.getResultSet();
+
+            while (rs.next()) {
+                user.setId(rs.getInt("userID"));
+                user.setfName(rs.getString("fName"));
+                user.setlName(rs.getString("lName"));
+                user.setCompany(rs.getInt("company"));
+                user.setPassword(rs.getString("password"));
+                user.setLocation(rs.getInt("location"));
+                user.setStatus(rs.getBoolean("status"));
+            }
+            return user;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+
+
 }
--- src/main/java/Usecase/UseCase.java
+++ src/main/java/Usecase/UseCase.java
@@ -1,4 +1,28 @@
 package Usecase;
+import DB.DBController;
+import Entity.User;
 
 public class UseCase {
+
+    DBController db = new DBController();
+
+    public User dbUser = new User();
+
+
+    public boolean checkInConfirm(String fName, String lName, String password) {
+
+        dbUser = db.getUser(fName,lName,password);
+
+        if (email.equals(dbUser.getEmail()) && password.equals(dbUser.getPassword() )) {
+            dbUser.setLogin(true);
+
+            //db.registerTime(hentetUser);
+
+
+            return true;
+
+        } else {
+            return false;
+        }
+    }
 }
