6ffce1a874de303baf8ceb3eda0ddc4042f56061
Wrenchx <wzs84ahh@gmail.com>
2023-05-10 11:14:42 +0200

    Initial commit

---
+++ src/main/java/com/example/LogistikfirmaEksamenFinal/LogistikfirmaEksamenFinalApplication.java
@@ -0,0 +1,13 @@
+package com.example.LogistikfirmaEksamenFinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogistikfirmaEksamenFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogistikfirmaEksamenFinalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/LogistikfirmaEksamenFinal/LogistikfirmaEksamenFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.LogistikfirmaEksamenFinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogistikfirmaEksamenFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
